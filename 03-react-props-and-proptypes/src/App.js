d3746cbc6e308b40a6db2e7e9fe21ac513c79bf8
Edwin Chen <edwinchen85@gmail.com>
2017-06-25 12:05:15 +0800

    Pass props:name into Greeting component

--- 03-react-props-and-proptypes/src/App.js
+++ 03-react-props-and-proptypes/src/App.js
@@ -4,15 +4,15 @@ const Headline = () => {
   return <h1 className="title">Welcome to the React world!</h1>
 }
 
-const Greeting = () => {
-  return <p>You will love it!</p>
+const Greeting = (props) => {
+  return <p>You will love it {props.name}!</p>
 }
 
 export const App = () => {
   return (
     <div>
       <Headline />
-      <Greeting />
+      <Greeting name="Edwin" />
     </div>
   )
 }
