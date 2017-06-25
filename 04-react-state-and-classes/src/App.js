ae2154a64c68408940a99482db4199f66331a350
Edwin Chen <edwinchen85@gmail.com>
2017-06-25 12:37:20 +0800

    Add constructor to App component

--- 04-react-state-and-classes/src/App.js
+++ 04-react-state-and-classes/src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export class App extends React.Component {
+  constructor(props) {
+    // super is a keyword that used to called functions on an object's parent
+    super(props);
+    this.state = {
+      buyItems: ['milk', 'bread', 'food']
+    }
+  }
+
   render() {
     return (
       <h1>Shopping List</h1>
