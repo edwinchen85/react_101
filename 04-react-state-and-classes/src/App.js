466e1925a1faf58babb61171b00d660d7d1f693b
Edwin Chen <edwinchen85@gmail.com>
2017-06-25 12:32:19 +0800

    Remove Headline and Greeting components

--- 04-react-state-and-classes/src/App.js
+++ 04-react-state-and-classes/src/App.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Headline = () => {
-  return <h1 className="title">Welcome to the React world!</h1>
-}
-
-const Greeting = (props) => {
-  const {name, age} = props;
-  return <p>You will love it {name} ({age})!</p>
-}
-
 export class App extends React.Component {
   render() {
     return (
-      <div>
-        <Headline />
-        <Greeting name="Edwin" age={25} />
-      </div>
+
     )
   }
-}
-
-Greeting.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
 }
