7920ad2546e3322c7e8019fdd328ae5d8f67dd8b
Edwin Chen <edwinchen85@gmail.com>
2017-06-25 11:59:50 +0800

    Add 03-react-props-and-proptypes starter files

---
+++ 03-react-props-and-proptypes/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className="title">Hello World!</h1>
+      <p>Testing rendering with JSX.</p>
+    </div>
+  )
+}
