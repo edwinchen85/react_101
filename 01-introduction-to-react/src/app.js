7dbdc413c2f176483e67dcf0fc3189c31669b14a
Edwin Chen <edwinchen85@gmail.com>
2017-06-25 11:15:14 +0800

    One parent element

--- 01-introduction-to-react/src/app.js
+++ 01-introduction-to-react/src/app.js
@@ -4,6 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>Hello World!</h1>,
+  <div><h1>Hello World!</h1><p>Test rendering</p></div>,
   document.getElementById('root')
 );
