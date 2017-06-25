5393ce1b414d2dc37986314b37c8a15846778649
Edwin Chen <edwinchen85@gmail.com>
2017-06-25 12:03:00 +0800

    Add Headline and Greeting components

--- 03-react-props-and-proptypes/src/App.js
+++ 03-react-props-and-proptypes/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+const Headline = () => {
+  return <h1 className="title">Welcome to the React world!</h1>
+}
+
+const Greeting = () => {
+  return <p>You will love it!</p>
+}
+
 export const App = () => {
   return (
     <div>
-      <h1 className="title">Hello World!</h1>
-      <p>Testing rendering with JSX.</p>
+      <Headline />
+      <Greeting />
     </div>
   )
 }
