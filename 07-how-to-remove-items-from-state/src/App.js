f79124313c7bdbf4b88a1adf46528e47e76a7599
Edwin Chen <edwinchen85@gmail.com>
2017-06-26 14:39:58 +0800

    Reindent after adding extra markup for positioning

--- 07-how-to-remove-items-from-state/src/App.js
+++ 07-how-to-remove-items-from-state/src/App.js
@@ -60,60 +60,64 @@ export class App extends React.Component {
 
     return (
       <div>
-        <header>
-          <h1>Shopping List</h1>
+        <div className="body">
+          <div className="container">
+            <header>
+              <h1>Shopping List</h1>
 
-          <form ref={input => this.addForm = input} className="form-inline" onSubmit={(e) => {this.addItem(e)}}>
-            <div className="form-group">
-              <label className="sr-only" htmlFor="newItemInput">Add New Item</label>
-              <input ref={input => this.newItem = input} type="text" placeholder="Bread" className="form-control" id="newItemInput" />
+              <form ref={input => this.addForm = input} className="form-inline" onSubmit={(e) => {this.addItem(e)}}>
+                <div className="form-group">
+                  <label className="sr-only" htmlFor="newItemInput">Add New Item</label>
+                  <input ref={input => this.newItem = input} type="text" placeholder="Bread" className="form-control" id="newItemInput" />
+                </div>
+                <button type="submit" className="btn btn-primary">Add</button>
+              </form>
+
+            </header>
+            <div className="content">
+              {
+                (message !== '' || buyItems.length === 0) && <p className="message text-danger">{message}</p>
+              }
+              {
+                buyItems.length > 0 &&
+                <table className="table">
+                  <caption>Shopping List</caption>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Item</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      buyItems.map(item => {
+                        return (
+                          <tr key={item}>
+                            <th scope="row">1</th>
+                            <td>{item}</td>
+                            <td className="text-right">
+                              <button onClick={(e) => this.removeItem(item)} type="button" className="btn btn-default btn-sm">
+                                Remove
+                              </button>
+                            </td>
+                          </tr>
+                        )
+                      })
+                    }
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <td colSpan="2">&nbsp;</td>
+                      <td className="text-right">
+                        <button onClick={(e) => this.clearAll()} className="btn btn-default btn-sm">Clear list</button>
+                      </td>
+                    </tr>
+                  </tfoot>
+                </table>
+              }
             </div>
-            <button type="submit" className="btn btn-primary">Add</button>
-          </form>
-
-        </header>
-        <div className="content">
-          {
-            (message !== '' || buyItems.length === 0) && <p className="message text-danger">{message}</p>
-          }
-          {
-            buyItems.length > 0 &&
-            <table className="table">
-              <caption>Shopping List</caption>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Item</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  buyItems.map(item => {
-                    return (
-                      <tr key={item}>
-                        <th scope="row">1</th>
-                        <td>{item}</td>
-                        <td className="text-right">
-                          <button onClick={(e) => this.removeItem(item)} type="button" className="btn btn-default btn-sm">
-                            Remove
-                          </button>
-                        </td>
-                      </tr>
-                    )
-                  })
-                }
-              </tbody>
-              <tfoot>
-                <tr>
-                  <td colSpan="2">&nbsp;</td>
-                  <td className="text-right">
-                    <button onClick={(e) => this.clearAll()} className="btn btn-default btn-sm">Clear list</button>
-                  </td>
-                </tr>
-              </tfoot>
-            </table>
-          }
+          </div>
         </div>
       </div>
     )
